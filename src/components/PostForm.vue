76e2f0b24718ef2cb7afac1e0cfd14c41cd2fa39
anastasia <azatylnikova@gmail.com>
2025-04-30 16:20:50 +0600

    first commit

---
+++ src/components/PostForm.vue
@@ -0,0 +1,74 @@
+<template>
+      <form @submit.prevent>
+        <!-- <input
+          v-bind:value="title"
+          @input="post.title = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Название"
+        />
+        <input
+          v-bind:value="body"
+          @input="post.body = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Описание"
+        /> -->
+        <input
+          v-model="post.title"
+          class="input"
+          type="text"
+          placeholder="Название"
+        />
+        <input
+          v-model="post.body"
+          class="input"
+          type="text"
+          placeholder="Описание"
+        />
+        <button class="btn" @click="createPost">Создать</button>
+      </form>
+  </template>
+  <script>
+  export default {
+    data() {
+      return {
+        post: {
+          title: '',
+          body: ''
+        }
+      }
+    },
+    methods: {
+      createPost() {
+        this.post.id = Date.new();
+        this.posts.push(newPost);
+        this.title= '';
+        this.body='';
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+  .btn {
+    align-self: flex-end;
+    margin-top: 15px;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+  }
+  .input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+    border: 1px solid teal;
+  }
+  </style>
+  
---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+      <div class="post" v-for="post in posts">
+        <div><strong>Название: </strong> {{ post.title }}</div>
+        <div><strong>Описание: </strong> {{ post.body }}</div>
+      </div>
+  </template>
+  <script>
+  export default {
+  props: {
+    posts :{
+      type: Array,
+      required: true
+    }
+  }
+  };
+  </script>
+  
+  <style>
+  .post {
+    padding: 15px;
+    border: 1px solid teal;
+    margin-top: 15px;
+  }
+ 
+  </style>
+  
